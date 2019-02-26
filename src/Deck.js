a5ae87d6378a0854dd383ae6f77b8d9776580823
Carl Hain <crhain@gmail.com>
2019-02-26 14:05:01 -0600

    Add spring animation to card deck when a card is swiped away

--- src/Deck.js
+++ src/Deck.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import { View, Animated, PanResponder, Dimensions } from 'react-native';
+import {
+  View,
+  Animated,
+  PanResponder,
+  Dimensions,
+  LayoutAnimation,
+  UIManager
+} from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.35 * SCREEN_WIDTH;
@@ -39,6 +46,13 @@ class Deck extends Component {
     this.position = position;
 
     this.state = { index: 0 };
+  }
+
+  componentWillUpdate() {
+    UIManager.setLayoutAnimationEnabledExperimental &&
+      UIManager.setLayoutAnimationEnabledExperimental(true);
+
+    LayoutAnimation.spring();
   }
 
   forceSwipe(direction) {
